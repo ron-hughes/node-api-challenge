8dabe9dd13b2f00d151c95cb6e07b410b18d85cd
U-THEOPHILUS\Ron <ronald.hughes1982@gmail.com>
2020-04-18 14:32:39 -0400

    almost done with actions router

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-/*
-play this: https://www.youtube.com/watch?v=d-diB65scQU
-
-Sing along:
-
-here's a little code I wrote, please read the README word for word, don't worry, you got this
-in every task there may be trouble, but if you worry you make it double, don't worry, you got this
-ain't got no sense of what is REST? just concentrate on learning Express, don't worry, you got this
-your file is getting way too big, bring a Router and make it thin, don't worry, be crafty
-there is no data on that route, just write some code, you'll sort it out… don't worry, just hack it…
-I need this code, but don't know where, perhaps should make some middleware, don't worry, just hack it
-
-Go code!
-*/
-
 const express = require('express');
+const actionsRouter = require('./routers/actionsRouter.js')
+const projectsRouter = require('./routers/projectsRouter.js')
 const server = express();
-const actionsRouter = ('../routers/action')
-server.use(express.json());
+const port = 4000;
 
+server.use(express.json());
+server.use("/api/actions", actionsRouter)
+server.use("/api/projects", projectsRouter)
+
+server.use((err, req, res, next) => {
+	console.log(err)
+	res.status(500).json({
+		message: "Something went wrong",
+	})
+})
+
+server.listen(port, ()=> {
+    console.log(`Server listening on port ${port}`)
+})
