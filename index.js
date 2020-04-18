4145bafc2aa3944efa5d38f6601fe4f62ee36834
U-THEOPHILUS\Ron <ronald.hughes1982@gmail.com>
2020-04-18 12:41:40 -0400

    started api project

--- index.js
+++ index.js
@@ -12,3 +12,9 @@ I need this code, but don't know where, perhaps should make some middleware, don
 
 Go code!
 */
+
+const express = require('express');
+const server = express();
+const actionsRouter = ('../routers/action')
+server.use(express.json());
+
